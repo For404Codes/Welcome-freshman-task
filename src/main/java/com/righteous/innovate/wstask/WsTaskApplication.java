2c417b7d88640f4d4738428f73c81850f9b5ad3d
For404Codes <zrj2214537@163.com>
2024-01-22 22:03:48 +0800

    大致框架（未测试，未添加数据库）

---
+++ src/main/java/com/righteous/innovate/wstask/WsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.righteous.innovate.wstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/righteous/innovate/wstask/WsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.righteous.innovate.wstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
