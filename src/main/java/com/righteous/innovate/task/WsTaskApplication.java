75d5a228c8f740885088c72aef853879cc89c526
For404Codes <zrj2214537@163.com>
2024-03-04 17:53:27 +0800

    （三个模块）依赖，配置，加入nacos管理

--- src/main/java/com/righteous/innovate/task/WsTaskApplication.java
+++ src/main/java/com/righteous/innovate/task/WsTaskApplication.java
@@ -2,8 +2,10 @@ package com.righteous.innovate.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WsTaskApplication {
 
